0e1f887855e0b08043f0a6a0e9d675b9d1f44f0e
Tyler Caelwaerts <tiecaelwaerts@gmail.com>
2025-03-03 16:20:51 -0600

    added components and made changes to main and app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import AboutMe from './components/AboutMe.jsx'
+import Portfolio from './components/Portfolio.jsx'
+import Contact from './components/Contact.jsx'
+import Resume from './components/Resume.jsx'
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/aboutme', element: <AboutMe /> },
+  { path: '/portfolio', element: <Portfolio /> },
+  { path: '/contact', element: <Contact /> },
+  { path: '/resume', element: <Resume /> },
+])
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />
 )
