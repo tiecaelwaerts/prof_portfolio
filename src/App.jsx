1bac37064b3f4abe57b7b97936653a41bebba2a7
Tyler Caelwaerts <tiecaelwaerts@gmail.com>
2025-03-04 22:01:54 -0600

    update names

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import Header from "./components/header";
-import Footer from "./components/footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 
 const App = () => {
